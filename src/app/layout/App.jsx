11de60712f6504d3fc08f9f5fe52f9260200a92f
Samet AKBAL <sametakbal7@gmail.com>
2023-01-26 22:37:19 +0300

    event crud operations

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,15 +1,30 @@
-import React,{useState} from "react";
-import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
-import NavBar from "../../features/nav/NavBar";
-import { Container } from "semantic-ui-react";
+import React, { useState } from 'react';
+import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar';
+import { Container } from 'semantic-ui-react';
 function App() {
   const [formOpen, setFormOpen] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
+  function handleSelectEvent(event) {
+    setSelectedEvent(event);
+    setFormOpen(true);
+  }
+  function handleCreateFormOpen() {
+    setSelectedEvent(null);
+    setFormOpen(true);
+  }
   return (
     <>
       <div className='App'>
-        <NavBar setFormOpen={setFormOpen}/>
+        <NavBar setFormOpen={handleCreateFormOpen} />
         <Container className='main'>
-          <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen}/>
+          <EventDashboard
+            formOpen={formOpen}
+            setFormOpen={setFormOpen}
+            selectEvent={handleSelectEvent}
+            selectedEvent={selectedEvent}
+          />
         </Container>
       </div>
     </>
